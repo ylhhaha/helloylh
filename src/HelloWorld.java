e287efcfaaee7ecefecf2325a061dfa69d621724
yan <1026386834@qq.com>
2021-04-09 11:13:53 +0800

    vs1.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这是我的第一个Git上传的项目 ");
-        System.out.println("这是我的第二个Git上传的项目      ");
+        System.out.println("这是我的第二个Git上传的项目. ");
 
     }
 }
